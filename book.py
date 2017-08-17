8c82f1aac8622ae90a9eaf0d9216622cfd149f9b
Ravi Sheth <r1sheth@ucds.edu>
2017-08-17 13:11:10 -0700

    finished commenting book.py

--- book.py
+++ book.py
@@ -1,3 +1,7 @@
+#Code simulates the probability that if a person retakes a class twice
+#then what are the chances that he/she gets the same textbook again
+
+
 #books if a person will get the same book
 from random import shuffle
 
@@ -5,10 +9,13 @@ students = input( "please enter the number of students in a class:\n")
 
 simulations = input( "please enter number of simulations:\n")
 
+#the two arrays that will hold the old and new classes
 classOne = []
 classTwo = []
 sameTotal = 0 #number of times the class had atleast one student with same book
 
+#the for loop will assign the old and shuffle the new class in order
+#to simulate a random assignment (same as shuffling textbooks)
 for index in range(students):
     classOne.append(index)
     classTwo.append(index)
@@ -17,11 +24,14 @@ shuffle(classTwo)
 
 for index in range(simulations):
 #
-    same = False    #if there is a book that is the same
+    same = False    
+    #if there is a student that got the same textbook
     for i in range(students):
     #
         if classOne[i] == classTwo[i]:
             same = True
+            #sameTotal+=1
+            #break
     #
     if same:
         sameTotal+=1
